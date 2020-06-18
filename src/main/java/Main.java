de82c60f0b126d31f005453f20e2afb62e211bae
Gaurav <gg.spark@gmail.com>
2020-06-18 16:15:27 +0530

    Added comments

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,9 +7,9 @@ import org.openqa.selenium.chrome.ChromeOptions;
  */
 public class Main {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "chromedriver.mac");
+        System.setProperty("webdriver.chrome.driver", "chromedriver.mac"); //Set chrome driver for mac in project dir, you can download latest from https://sites.google.com/a/chromium.org/chromedriver/downloads
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("user-data-dir=~/Library/Application Support/Google/Chrome/Default/");
+        options.addArguments("user-data-dir=~/Library/Application Support/Google/Chrome/Default/"); //Setting this to use the same account again and again without login
         WebDriver driver = new ChromeDriver(options); //Create web driver
         FacebookClient facebookClient = new FacebookClient(driver); //Create fb client obj with driver
         facebookClient.wishBirthday(); //Wish birthday
