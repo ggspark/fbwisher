8cfbbb296f32966b74aed60877908bc5c95ca30f
gaurav <gaurav@no-reply.com>
2020-12-29 01:06:04 +0530

    Updated unfriend search with friendslist

--- findFacebookUnfriends.sh
+++ findFacebookUnfriends.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
-# Cd to the directory downloaded from facebook data export
+# Download facebook information with friends and messages selected https://www.facebook.com/dyi
+# Cd to the directory downloaded from facebook
 # bash findFacebookUnfriends.sh | grep title
 SECONDS=0
 
 for i in $(find ~/Downloads/facebook-messages -name '*.json' -type f); do
-  cat "$i" | jq 'if (.messages[0].content == "Happy\nBirthday\n:)") and (.messages[0].sender_name == "Gaurav Gupta") and (.messages | length) < 5 then {name: .participants[0].name, title: .title} else null end'
+  MESSENGER_NAME=$(cat "$i" | jq 'if (.messages[0].content == "Happy\nBirthday\n:)") and (.messages[0].sender_name == "Gaurav Gupta") and (.messages | length) < 5 then .participants[0].name else null end')
+  grep "$MESSENGER_NAME" ./friends/friends.json
 done
 
 # Complete
