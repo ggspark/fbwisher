dad76c09ae4145210f6439f1f44bdb80055ed82f
gaurav <gaurav@no-reply.com>
2020-12-29 01:08:55 +0530

    Updated findfbunfriend

--- findFacebookUnfriends.sh
+++ findFacebookUnfriends.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 # Download facebook information with friends and messages selected https://www.facebook.com/dyi
 # Cd to the directory downloaded from facebook
-# bash findFacebookUnfriends.sh | grep title
+# bash findFacebookUnfriends.sh
 SECONDS=0
 
 for i in $(find ~/Downloads/facebook-messages -name '*.json' -type f); do
-  MESSENGER_NAME=$(cat "$i" | jq 'if (.messages[0].content == "Happy\nBirthday\n:)") and (.messages[0].sender_name == "Gaurav Gupta") and (.messages | length) < 5 then .participants[0].name else null end')
+  MESSENGER_NAME=$(cat "$i" | jq 'if
+    (.messages[0].content == "Happy\nBirthday\n:)") and
+    (.messages | length) < 5
+    then .participants[0].name else null end')
   grep "$MESSENGER_NAME" ./friends/friends.json
 done
 
